256dc50e66ed73ed652e5bd9ac170ff8a89ee5e4
denismwau <dennismwau33@gmail.com>
2019-12-05 14:39:47 +0300

    modified html file

--- js/script .js
+++ js/script .js
@@ -1,112 +1,62 @@
-
 //declaring arrays
 
-var daysOfWeek=[
+var daysOfWeek = [
+  "Sunday",
 
-    "Sunday",
+  "Monday",
 
-    "Monday",
+  "Tuesday",
 
-    "Tuesday",
+  "Wednesday",
 
-    "Wednesday",
+  "Thursday",
 
-    "Thursday",
+  "Friday",
 
-    "Friday",
-
-    "Saturday",
-
+  "Saturday"
 ];
 
+var maleNames = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
 
-
-var maleNames=[
-
-    "Kwasi",
-
-    "Kwadwo",
-
-    "Kwabena",
-
-    "Kwaku",
-
-    "Yaw",
-
-    "Kofi",
-
-    "Kwame",
-
-];
-
-
-
-var femaleNames=[
-
-    "Akosua",
-
-    "Adwoa",
-
-    "Abenaa",
-
-    "Akua",
-
-    "Yaa",
-
-    "Afua",
-
-    "Ama",
-
-];
-
-
+var femaleNames = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
 
 //calling functions
 
-function getName(){
+function getAkanName() {
+  // Collect data from input fields
+  var formData = document.forms[0];
 
-var year=parseInt(document.getElementById("year").value);
+  var year = parseInt(formData.year.value);
+  var month = parseInt(formData.month.value);
+  var day = parseInt(formData.day.value);
 
-var month =parseInt(document.getElementById("month").value);
+  var male = document.getElementById("male");
+  var female = document.getElementById("female");
 
-var day=parseInt(document.getElementById("day").value);
+  //validation
 
-var male = document.getElementById("male");
+  if (day <= 0 || day > 31) {
+    alert("Oops!please enter a valid day");
+  } else if (month <= 0 || month > 12 || (month == 2 && day > 29)) {
+    alert("Oops!please enter a valid month");
+  }
 
-var female = document.getElementById("female");
+  var day = new Date(year + "/" + month + "/" + day);
+  var birthDay = day.getDay();
 
-
-
-//validation
-
-if(day<=0||day>31)
-
-alert("Oops!please enter a valid day");
-
-else if(month<0||month>12 ||month ==2 && day>29)
-
-alert("Oops!please enter a valid month");
-
-
-
-
-
-var day = new Date(year + "/" + month + "/" + day);
-
-
-
-var birthDay=day.getDay();
-
-if(male.checked==true){
-
-   alert("You were born on " +daysOfWeek[birthDay]+ " and your akan name is " + maleNames[birthDay])
-
-}
-
-else if(female.checked==true){
-
-   alert("You were born on " +daysOfWeek[birthDay]+ " and your akan name is " + femaleNames[birthDay])
-
-}
-
+  if (male.checked == true) {
+    alert(
+      "You were born on " +
+        daysOfWeek[birthDay] +
+        " and your akan name is " +
+        maleNames[birthDay]
+    );
+  } else if (female.checked == true) {
+    alert(
+      "You were born on " +
+        daysOfWeek[birthDay] +
+        " and your akan name is " +
+        femaleNames[birthDay]
+    );
+  }
 }
